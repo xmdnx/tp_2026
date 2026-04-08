daa9d038c862410ae87c758ed878f30921e7ef55
mrchudak <gleb.ts2104@gmail.com>
2026-04-08 23:10:45 +0300

    fix(composite.h): name of the scale function argument

--- tsuremichev.gleb/T4/composite_shape.h
+++ tsuremichev.gleb/T4/composite_shape.h
@@ -13,7 +13,7 @@ public:
   double getArea() const override;
   Point getCenter() const override;
   void move(double dx, double dy) override;
-  void scale(double coefficient) override;
+  void scale(double koef) override;
   std::string getName() const override;
   void print() const override;
 
